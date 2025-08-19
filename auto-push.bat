d86e8e2e7d251b09bc08c2a2d95f726706e3d569
vruttantlihitkar-sys <vruttantlihitkar@gmail.com>
2025-08-19 22:11:53 +0530

    Committed from Vruttant's PC on 19-08-2025 22:11:53.94

--- auto-push.bat
+++ auto-push.bat
@@ -1,5 +1,14 @@
+@echo off
 cd /d V:\git-dsa
+
+:: Add all changes
 git add .
-git commit -m "Auto update %date% %time%"
+
+:: Commit with custom message (Vruttant's PC + date & time)
+git commit -m "Committed from Vruttant's PC on %date% %time%"
+
+:: Pull remote changes before push
 git pull origin main --rebase
+
+:: Push to GitHub
 git push origin main
---
+++ class.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i,x;
+    
+   if (x/5==0){
+    
+   }
+}
