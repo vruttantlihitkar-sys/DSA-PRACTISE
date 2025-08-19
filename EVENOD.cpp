bc9b415aa673ff1750f0a667bf45a700e48d4624
vruttantlihitkar-sys <vruttantlihitkar@gmail.com>
2025-08-19 21:47:14 +0530

    Auto update 19-08-2025 21:47:14.54

---
+++ EVENOD.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int x;
+    cin>>x;
+
+    if (x%2==0){
+        cout<<"even hai lavdu";
+    }
+    else {
+        cout<<"odd hai bisi";
+    }
+
+    return 0;
+}
