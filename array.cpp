f1c9340501ca67e1781150262fc1b2c33333594c
vruttantlihitkar-sys <vruttantlihitkar@gmail.com>
2025-08-19 20:34:22 +0530

    initial commit

---
+++ array.cpp
@@ -0,0 +1,14 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int arrr[100];
+    for (int i=0;i<=5;i++){
+        cin>>arrr[i];
+    }
+      for (int i=0;i<=5;i++){
+        cout<<arrr[i]<<" ";
+    }
+    return 0;
+}
